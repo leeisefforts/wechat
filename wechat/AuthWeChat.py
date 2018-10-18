0aa6f07320fba5e20865458bfcc68b2f3bfef69a
bryant <leeisefforts@163.com>
2018-10-18 10:16:19 +0800

    addd

--- wechat/AuthWeChat.py
+++ wechat/AuthWeChat.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify
-from common.Helper import WeChatService, xml_to_dict, getCurrentDate
+from flask import Blueprint, jsonify, request
+from common.Helper import WeChatService, xml_to_dict, getCurrentDate, dict_to_xml
 from common.libs.followers import FollowerSevice
 from common.libs.SignIn import SignInService
 from application import db, app
 from common.modals.ConversationLog import ConversationLog
-import requests, json
+import requests, json, time
 
 route_auth = Blueprint('wechat_page', __name__)
 
@@ -32,19 +32,15 @@ def authWeChat():
 
 '''
 
-<xml>  
-<ToUserName>< ![CDATA[toUser] ]></ToUserName>  <FromUserName>< ![CDATA[fromUser] ]>
-</FromUserName>  <CreateTime>1348831860</CreateTime>  <MsgType>< ![CDATA[text] ]></MsgType>  
-<Content>< ![CDATA[this is a test] ]></Content>  <MsgId>1234567890123456</MsgId>  
-</xml>
+'<xml>  <ToUserName>test</ToUserName>  <FromUserName>oCMdfwKnqFIOhC6FxV3nG9KuEiUA</FromUserName> ' \
+          '<CreateTime>1348831860</CreateTime>  <MsgType>text</MsgType>  <Content>' \
+          '签到</Content>  <MsgId>1234567890123456</MsgId> </xml>'
 '''
 
 
 @route_auth.route('/wechat_msg')
 def wechat_msg():
-    xml = '<xml>  <ToUserName>test</ToUserName>  <FromUserName>oCMdfwKnqFIOhC6FxV3nG9KuEiUA</FromUserName> ' \
-          '<CreateTime>1348831860</CreateTime>  <MsgType>text</MsgType>  <Content>' \
-          '签到</Content>  <MsgId>1234567890123456</MsgId> </xml>'
+    xml = request.values
     code = xml_to_dict(xml)
     str = ''
     # 判断openid是否存在
@@ -64,4 +60,14 @@ def wechat_msg():
         db.session.add(cl)
         db.session.commit()
 
-    return str
+    tm = int(time.time())
+    result = {
+        'ToUserName': code['FromUserName'],
+        'FromUserName': code['ToUserName'],
+        'CreateTime': tm,
+        'MsgType': 'text',
+        'Content': str
+    }
+    result = dict_to_xml(result)
+
+    return result
